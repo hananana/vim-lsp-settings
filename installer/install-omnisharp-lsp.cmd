5a1f29d65de6cefaeea3964971c6d91c007163a3
hananana <hananana.vimmer@gmail.com>
2021-04-23 10:20:46 +0900

    :hammer: update omnisharp version

--- installer/install-omnisharp-lsp.cmd
+++ installer/install-omnisharp-lsp.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=1.37.5
+set VERSION=1.37.8
 curl -L -o omnisharp-win-x64.zip "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v%VERSION%/omnisharp-win-x64.zip"
 call "%~dp0\run_unzip.cmd" omnisharp-win-x64.zip
 del omnisharp-win-x64.zip
